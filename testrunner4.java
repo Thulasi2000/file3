61b0b70e6746e2a2f15b3101fd128ae80fa93af2
Thulasi2000 <thulasi333ram@gmail.com>
2022-10-15 12:01:24 +0530

    file

---
+++ testrunner4.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("runner")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "runner")
+@RunWith(JUnitPlatform.class)
+public class testrunner4 {
+    
+}
+
+
+
